5c2ebcfa3bc33a8c58c0eff7627d8628ea1836b4
llull179 <lluisllullriera@gmail.com>
2021-03-28 23:45:26 +0200

    Preparació

---
+++ CjtUsuarios.hh
@@ -0,0 +1,35 @@
+/** @file CjtUsuarios.hh
+    @brief Especificació de la classe CjtUsuarios
+*/
+
+#ifndef _CJTUSUARIS_HH_
+#define _CJTUSUARIS_HH_
+
+#include "Usuario.hh"
+
+/** @class CjtUsuarios
+    @brief Representa el conjunto de todos los usuarios
+    . 
+
+    Contiene Usuarios. 
+*/
+
+//Constructoras
+
+/** @brief Creadora por defecto. 
+
+      Se ejecuta automáticamente al declarar el conjunto de usuarios.
+      \pre <em>cierto</em>
+      \post El resultado es un conjunto de usuarios.
+  */ 
+ CjtUsuarios();
+ 
+
+//Consultores
+
+
+//Modificadores
+
+void añadirUsuario();
+
+#endif
---
+++ program.cc
@@ -0,0 +1,31 @@
+#include "CjtUsuaris.hh"
+#include "Curs.hh"
+using namespace std;
+
+int main() {
+    string op;
+    int P;
+    for(int i = 0; i < P; ++i) {
+
+    }
+    int Q;
+    for(int i = 0; i < Q; ++i) {
+        
+    }
+    int N;
+    for(int i = 0; i < N; ++i) {
+        
+    }
+    int M;
+    for(int i = 0; i < M; ++i) {
+        
+    }
+
+    cin >> op;
+    while(op != "fin") {
+        
+        if(op ==)
+
+        cin >> op;
+    }
+}
